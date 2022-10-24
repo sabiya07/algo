1d59ca92c5de2c3fecdc45626a930cf6f37ae6c4
sabiya07 <mohammedsabiya2@gmail.com>
2022-10-24 21:00:07 +0530

    algorithms

---
+++ practice-1/p.js
@@ -0,0 +1,67 @@
+
+
+
+// let a=9
+// if(a%2==0){
+//     console.log("it is even");
+// }
+// else{
+//     console.log("it is odd")
+// }
+
+// a=[23,12,2,4,67,89,66]
+// s=0
+//     for(i=0;i<Array.length;i++){
+// s=a+a[i]+""
+// console.log(s)
+//     }
+
+// function max(a)
+// Math.max.app\
+
+
+// function greatest(a,b,c){
+// if(a>b,c){
+//     console.log("a is the greatest")
+// }else if(b>a,c){
+//     console.log("b is the  greatest")
+
+// }else if (c>a,b){
+//     console.log("c is the greatest")
+// }
+// }
+// greatest (5,2,8)
+
+
+
+// function isCharacterALetter(char) {
+//     return (/[a-zA-Z]/).test(char)
+//   }
+//   console.log(isCharacterALetter("s"))
+
+//------------------------------------------//
+
+//Write a program to find the Sum of Array Elements.
+
+
+// var a=[1,2,3,4,4]
+// sum=0
+
+//   for(i=0;i<a.length;i++){
+     
+//     sum=sum + a[i]
+//   }
+
+
+// console.log(sum)
+
+//------------------------------------------//
+
+//Write a program to find the maximum number in an array using function
+
+let Array=[12,90,40,2,3,999]
+function maximum(array){
+  for (i=0;i<=array.length;i++){
+    
+  }
+}
